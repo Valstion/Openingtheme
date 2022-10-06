f37364157c1cf5e828dee51151558f4378c3fb11
Tohanol <lordvalstion@gmail.com>
2022-10-06 16:05:21 +0300

    Первые 2 теста

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,27 @@ public class Main {
         System.out.println("Hello world!");
         DayOfWeek actualWeekDay = LocalDate.now().getDayOfWeek();
         System.out.println(actualWeekDay);
+        try {
+
+            int i;
+            for (i = 0; i < 5; i++) {
+                Thread.sleep(1000);
+                System.out.println("Timer" + (i + 1));
+            }}
+         catch (InterruptedException e){
+
+            System.out.println("Interupted exseption invoced");}
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
+
+
+
+
+
+
--- src/test/java/introduction/Classwork.java
+++ src/test/java/introduction/Classwork.java
@@ -1,9 +1,63 @@
 package introduction;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static java.util.Calendar.MONDAY;
+
 public class Classwork {
-  //  public class void(){
+    //  public class void(){
 
 
-   // }
+    // }
+
+    @Test
+
+    public void testGreenifDividedBy() {
+        //duomenys testo veiksmas palyginimas
+
+
+        int actualInput = 995;
+        int expectedRezults = 0;
+        int actualRezults;
+
+        //testo veiksmas
+        actualRezults = actualInput % 3;
+
+        //palyginimas
+        Assert.assertTrue(actualRezults == expectedRezults,
+                "Actual: " + actualRezults + ", Expected: " + expectedRezults);
+    }
+
+
+    @Test
+    public void testifDayOfTheWEEK() {
+        //duomenys
+        DayOfWeek expectedWeekDay = DayOfWeek.THURSDAY;
+        //Test veiksmas
+        DayOfWeek actualWeekDay = LocalDate.now().getDayOfWeek();
+        boolean isTheSame = actualWeekDay == expectedWeekDay;
+
+        //Palyginimas
+        Assert.assertTrue(isTheSame, String.format("Expected: %s, Acutal: %s", expectedWeekDay, actualWeekDay));
+
+    }
+
+    @Test
+    public void testCount() {
+        int expectedEven = 4;
+        int actualNumber = 0;
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                actualNumber++;
+            }
+        }
+        Assert.assertEquals(expectedEven, actualNumber);
+    }
+
 
 }
