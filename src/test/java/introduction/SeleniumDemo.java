8585b701b5909ef6ee5cb551c362296e66fdfd8c
Tohanol <lordvalstion@gmail.com>
2022-10-07 10:56:59 +0300

    Web Driver Torso

--- src/test/java/introduction/SeleniumDemo.java
+++ src/test/java/introduction/SeleniumDemo.java
@@ -2,7 +2,9 @@ package introduction;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.OperaDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chromium.ChromiumDriver;
 import org.testng.annotations.Test;
@@ -14,7 +16,7 @@ public class SeleniumDemo {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.vilniuscoding.lt/");
         try {
-        driver.manage().window().maximize();
+            driver.manage().window().maximize();
             Thread.sleep(3000);
             driver.manage().window().fullscreen();
             Thread.sleep(3000);
@@ -26,4 +28,18 @@ public class SeleniumDemo {
 
     }
 
+    @Test
+    public void testVlickKontaktaiOfVCsPage() {
+        WebDriverManager.operadriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.vilniuscoding.lt/");
+        WebElement linkKontaktai = driver.findElement(By.id("menu-item-3969"));
+        linkKontaktai.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
 }
